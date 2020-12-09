b7133fcda9fa5fa09351e9b3606447d0bf98f6e3
jmoussalli <jerome.moussalli@gmail.com>
2020-12-09 17:37:46 +0100

    Spring Configuration examples.

---
+++ src/main/java/guru/springframework/config/GreetingServiceConfig.java
@@ -0,0 +1,44 @@
+package guru.springframework.config;
+
+import guru.springframework.services.GreetingRepository;
+import guru.springframework.services.GreetingService;
+import guru.springframework.services.GreetingServiceFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * Package :  guru.config
+ * Created by jerom on 09/12/2020 at 13:40
+ */
+@Configuration
+public class GreetingServiceConfig {
+
+    @Bean
+    GreetingServiceFactory greetingServiceFactory(GreetingRepository repository) {
+        return new GreetingServiceFactory(repository);
+    }
+
+    @Bean
+    @Primary
+    @Profile({"default", "en"})
+    GreetingService primaryGreetingService(GreetingServiceFactory greetingServiceFactory){
+        return greetingServiceFactory.createGreetingService("en");
+    }
+
+    @Bean
+    @Primary
+    @Profile("es")
+    GreetingService primarySpanishGreetingService(GreetingServiceFactory greetingServiceFactory){
+        return greetingServiceFactory.createGreetingService("es");
+    }
+
+    @Bean
+    @Primary
+    @Profile("de")
+    GreetingService primaryGermanGreetingService(GreetingServiceFactory greetingServiceFactory){
+        return greetingServiceFactory.createGreetingService("de");
+    }
+
+}
---
+++ src/main/java/guru/springframework/services/GreetingServiceFactory.java
@@ -0,0 +1,28 @@
+package guru.springframework.services;
+
+/**
+ * Package :  guru.services
+ * Created by jerom on 09/12/2020 at 13:36
+ */
+public class GreetingServiceFactory {
+
+    private GreetingRepository greetingRepository;
+
+    public GreetingServiceFactory(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
+
+    public GreetingService createGreetingService(String lang) {
+
+        switch (lang) {
+            case "en":
+                return new PrimaryGreetingService(greetingRepository);
+            case "de":
+                return new PrimaryGermanGreetingService(greetingRepository);
+            case "es":
+                return new PrimarySpanishGreetingService(greetingRepository);
+            default:
+                return new PrimaryGreetingService(greetingRepository);
+        }
+    }
+}
