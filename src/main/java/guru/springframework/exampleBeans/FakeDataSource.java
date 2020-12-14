c031c8e5d03f0e36c59ee74e6cb9272c1aa402b1
jmoussalli <jerome.moussalli@gmail.com>
2020-12-14 12:26:30 +0100

    Spring Environment properties.

---
+++ src/main/java/guru/springframework/exampleBeans/FakeDataSource.java
@@ -0,0 +1,35 @@
+package guru.springframework.exampleBeans;
+
+/**
+ * Package :  guru.springframework.exampleBeans
+ * Created by jerom on 12/12/2020 at 16:50
+ */
+public class FakeDataSource {
+    private String user;
+    private String password;
+    private String url;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
